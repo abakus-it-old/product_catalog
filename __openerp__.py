ce07423b6bdf4a6f0696d3b4bce71cf17fe9f889
Valentin THIRION <valentinthirion@gmail.com>
2015-02-26 10:25:13 +0100

    Edited description

--- __openerp__.py
+++ __openerp__.py
@@ -2,12 +2,10 @@
     'name': "Products catalog with prices",
     'version': '1.0',
     'depends': ['sale', 'product'],
-    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'author': "Valentin THIRION & Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
     'category': 'Sale',
-    'description': """This modules adds some functionalities to the contracts for AbAKUS.
-
-It adds a list of products in the sale application with the computed sale price regarding pricelists, supplier prices etc.
+    'description': """This module adds a list of products in the sale application with the computed sale price regarding pricelists, supplier prices etc.
 It also colors tghe lines in order to improve visibility.
 
 This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
