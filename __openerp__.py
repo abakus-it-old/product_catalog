be180bbe55d2d2ee5ef1f07b863d3486a3fc6194
Valentin THIRION <valentinthirion@gmail.com>
2015-02-26 10:09:41 +0100

    First working version with colors and prices

---
+++ __openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Products catalog with prices",
+    'version': '1.0',
+    'depends': ['sale', 'product'],
+    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Sale',
+    'description': """
+    Creates a list view with all the products and their computed prices regarding their default pricelist (category).
+    """,
+    'data': ['product_pricelist.xml',],
+}
