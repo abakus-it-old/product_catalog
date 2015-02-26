64c003c933599696332626b7f5e63b1884744b98
Valentin THIRION <valentinthirion@gmail.com>
2015-02-26 10:14:39 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This modules adds some functionalities to the contracts for AbAKUS.
+
+It adds a list of products in the sale application with the computed sale price regarding pricelists, supplier prices etc.
+It also colors tghe lines in order to improve visibility.
+
+This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
--- __openerp__.py
+++ __openerp__.py
@@ -5,8 +5,12 @@
     'author': "Valentin THIRION, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
     'category': 'Sale',
-    'description': """
-    Creates a list view with all the products and their computed prices regarding their default pricelist (category).
+    'description': """This modules adds some functionalities to the contracts for AbAKUS.
+
+It adds a list of products in the sale application with the computed sale price regarding pricelists, supplier prices etc.
+It also colors tghe lines in order to improve visibility.
+
+This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
     """,
     'data': ['product_pricelist.xml',],
 }
